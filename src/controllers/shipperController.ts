56dad6630c94ec78a67208606b87683f6a361a21
Pratham_Mishra04 <prathamm0409@gmail.com>
2023-05-14 12:06:40 +0530

    feat: OTP logic added

--- src/controllers/shipperController.ts
+++ src/controllers/shipperController.ts
@@ -1,6 +1,8 @@
 import catchAsync from '../managers/catchAsync';
 import { Request, Response, NextFunction } from 'express';
 import Order from '../models/orderModel';
+import AppError from '../managers/AppError';
+import * as otpGenerator from 'otp-generator';
 
 export const getOrders = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
@@ -53,6 +55,57 @@ export const confirmOTW = catchAsync(
         const order = await Order.findById(req.params.orderID);
 
         order.status = 2;
+
+        await order.save();
+
+        res.status(200).json({
+            status: 'success',
+            requestedAt: req.requestedAt,
+            order,
+        });
+    }
+);
+
+export const sendOTP = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const order = await Order.findById(req.params.orderID);
+
+        order.shippingDetails.OTP = otpGenerator.generate(6, {
+            digits: true,
+            lowerCaseAlphabets: false,
+            upperCaseAlphabets: false,
+            specialChars: false,
+        });
+        order.shippingDetails.optExpiration = new Date(
+            Date.now() + 10 * 60 * 60
+        ); //10 mins
+
+        await order.save();
+
+        //send OTP
+
+        res.status(200).json({
+            status: 'success',
+            requestedAt: req.requestedAt,
+            order,
+        });
+    }
+);
+
+export const verifyOTP = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const order = await Order.findById(req.params.orderID);
+
+        if (order.isOTPExpired(Date.now()))
+            return next(
+                new AppError('OTP has Expired, please generate a new one', 400)
+            );
+        if (!order.isCorrectOTP(req.body.OTP))
+            return next(new AppError('Incorrect OTP', 400));
+
+        order.status = 3;
+        order.isActive = false;
+        order.delivery.deliveredAt = new Date();
 
         await order.save();
 
--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -1,111 +1,133 @@
 import mongoose from 'mongoose';
+import * as bcrypt from 'bcryptjs';
 
-export interface ProductObj{
-    productName:string;
-    productPrice:number;
+export interface ProductObj {
+    productName: string;
+    productPrice: number;
 }
 
-export interface PriceDetails{
-    basePrice:number;
-    discount:number;
-    deliveryCharges:number;
-    totalAmount:number
+export interface PriceDetails {
+    basePrice: number;
+    discount: number;
+    deliveryCharges: number;
+    totalAmount: number;
 }
 
-export interface Delivery{
-    receiverName:string;
-    receiverPhoneNumber:number;
-    block:string;
-    roomNumber:number;
-    deliveryInstructions:string;
-    isDelivered:boolean
-    deliveredAt:Date;
+export interface Delivery {
+    receiverName: string;
+    receiverPhoneNumber: number;
+    block: string;
+    roomNumber: number;
+    deliveryInstructions: string;
+    isDelivered: boolean;
+    deliveredAt: Date;
 }
 
-export interface ShippingDetails{
-    shipper:mongoose.Schema.Types.ObjectId;
-    deliveryAcceptedAt:Date;
-    opt:string;
-    optExpiration:Date
+export interface ShippingDetails {
+    shipper: mongoose.Schema.Types.ObjectId;
+    deliveryAcceptedAt: Date;
+    OTP: string;
+    optExpiration: Date;
 }
 
 export interface OrderDocument extends mongoose.Document {
-    placedBy:mongoose.Schema.Types.ObjectId;
-    cart:ProductObj[];
-    instructions:string;
-    priceDetails:PriceDetails;
-    delivery:Delivery
-    shippingDetails:ShippingDetails;
+    placedBy: mongoose.Schema.Types.ObjectId;
+    cart: ProductObj[];
+    instructions: string;
+    priceDetails: PriceDetails;
+    delivery: Delivery;
+    shippingDetails: ShippingDetails;
     createdAt: Date;
-    isActive:boolean;
-    isAccepted:boolean;
-    status:number
+    isActive: boolean;
+    isAccepted: boolean;
+    status: number;
+    isCorrectOTP(inOTP: string): Promise<boolean>;
+    isOTPExpired(timestrap:number):boolean
 }
 
 const orderSchema = new mongoose.Schema(
     {
-        placedBy:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'User'
+        placedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
         },
-        cart:[
+        cart: [
             {
-                productName:String,
-                productPrice:Number
-            }
+                productName: String,
+                productPrice: Number,
+            },
         ],
-        instructions:String,
-        priceDetails:{
-            basePrice:Number,
-            discount:Number,
-            deliveryCharges:Number,
-            totalAmount:Number
+        instructions: String,
+        priceDetails: {
+            basePrice: Number,
+            discount: Number,
+            deliveryCharges: Number,
+            totalAmount: Number,
         },
-        delivery:{
-            receiverName:String,
-            receiverPhoneNumber:Number,
-            block:String,
-            roomNumber:Number,
-            deliveryInstructions:String,
-            isDelivered:{
-                type:Boolean,
-                default:false
+        delivery: {
+            receiverName: String,
+            receiverPhoneNumber: Number,
+            block: String,
+            roomNumber: Number,
+            deliveryInstructions: String,
+            isDelivered: {
+                type: Boolean,
+                default: false,
             },
-            deliveredAt:Date
+            deliveredAt: Date,
         },
-        shippingDetails:{
-            shipper:{
-                type:mongoose.Schema.Types.ObjectId,
-                ref:'User'
+        shippingDetails: {
+            shipper: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
             },
-            deliveryAcceptedAt:Date,
-            opt:String,
-            optExpiration:Date
+            deliveryAcceptedAt: Date,
+            OTP: String,
+            optExpiration: Date,
         },
-        isActive:{
-            type:Boolean,
-            default:true
+        isActive: {
+            type: Boolean,
+            default: true,
         },
-        isAccepted:{
-            type:Boolean,
-            default:false
+        isAccepted: {
+            type: Boolean,
+            default: false,
         },
-        status:{
-            type:Number,
-            enum:[-1,0,1,2,3],  // -1:notAccepted,  0: accepted, 1: pickedUp, 2:on the way, 3:delivered
-            default:-1
+        status: {
+            type: Number,
+            enum: [-1, 0, 1, 2, 3], // -1:notAccepted,  0: accepted, 1: pickedUp, 2:on the way, 3:delivered
+            default: -1,
         },
-        createdAt:{
-            type:Date,
-            default:Date.now()
+        createdAt: {
+            type: Date,
+            default: Date.now(),
         },
-        
     },
     {
         toJSON: { virtuals: true },
         toObject: { virtuals: true },
     }
 );
+
+orderSchema.pre('save', async function (next) {
+    if (!this.isModified('shippingDetails.OTP')) return next();
+    this.shippingDetails.OTP = await bcrypt.hash(this.shippingDetails.OTP, 12);
+    next();
+});
+
+orderSchema.methods.isCorrectOTP = async function (
+    inOTP: string
+): Promise<boolean> {
+    const order = this as OrderDocument;
+    return await bcrypt.compare(inOTP, order.shippingDetails.OTP);
+};
+
+orderSchema.methods.isOTPExpired = function (timestrap: number): boolean {
+    const order = this as OrderDocument;
+    const OTPTimestrap: number =
+        Number(order.shippingDetails.optExpiration.getTime()) / 1000;
+    return timestrap > OTPTimestrap;
+};
 
 const Order = mongoose.model<OrderDocument>('Order', orderSchema);
 
